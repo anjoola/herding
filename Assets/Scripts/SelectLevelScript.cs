47d27f8de47c04b3b22ca8885c9bcf89a6bc4f3e
Angela Gong <anjoola@anjoola.com>
2015-02-22 00:08:14 -0800

    Connected main menu with world map and cow palace level

--- Assets/Scripts/SelectLevelScript.cs
+++ Assets/Scripts/SelectLevelScript.cs
@@ -7,12 +7,19 @@ using System.Collections;
 public class SelectLevelScript : MonoBehaviour {
 	float SCALE_FACTOR = 1.1f;
 	float SCALE_TIME = 1.0f;
+	float CLICK_THRESHOLD = 100f;
+
+	Vector3 cowPalaceLevel;
 
 	void Start () {
-	
+		cowPalaceLevel = Camera.main.WorldToScreenPoint(GameObject.Find ("Cow Palace Level").transform.position);
 	}
 	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			Vector3 mousePos = Input.mousePosition;
 
+			MaybeLoadLevel (mousePos, cowPalaceLevel, "CowPalace");
+		}
 	}
 	void OnMouseEnter() {
 		iTween.ScaleBy (gameObject, iTween.Hash (
@@ -27,5 +34,19 @@ public class SelectLevelScript : MonoBehaviour {
 			"y", 1/SCALE_FACTOR,
 			"z", 1/SCALE_FACTOR,
 			"time", SCALE_TIME));
+	}
+
+	/**
+	 * Possibly loads a level if the user clicks on the right spot.
+	 *
+	 * mousePos: Where the use clicked.
+	 * levelPos: Location of the on-screen object representing the level.
+	 * levelName: Name of the level.
+	 */
+	void MaybeLoadLevel(Vector3 mousePos, Vector3 levelPos, string levelName) {
+		if (Mathf.Abs (levelPos.x - mousePos.x) < CLICK_THRESHOLD &&
+		    Mathf.Abs (levelPos.y - mousePos.y) < CLICK_THRESHOLD) {
+			Application.LoadLevel (levelName);
+		}
 	}
 }
