6b75469455b054aea21c76943b228b977c0b1d12
Angela Gong <anjoola@anjoola.com>
2015-02-22 14:40:02 -0800

    Made world map zoom feature

--- Assets/Scripts/SelectLevelScript.cs
+++ Assets/Scripts/SelectLevelScript.cs
@@ -5,22 +5,37 @@ using System.Collections;
  *  TODO documentation
  */
 public class SelectLevelScript : MonoBehaviour {
-	float SCALE_FACTOR = 1.1f;
-	float SCALE_TIME = 1.0f;
+	// Transition duration from one camera position to another.
+	float TRANSITION_DURATION = 0.8f;
+
+	// Camera position and rotation.
+	Vector3 cameraOrigPos;
+	Quaternion cameraOrigRot;
+	
 	float CLICK_THRESHOLD = 100f;
 
-	Vector3 cowPalaceLevel;
+	//float SCALE_FACTOR = 1.1f;
+	//float SCALE_TIME = 1.0f;
 
 	void Start () {
-		cowPalaceLevel = Camera.main.WorldToScreenPoint(GameObject.Find ("Cow Palace Level").transform.position);
+		// Get original camera orientation.
+		cameraOrigPos = Camera.main.transform.position;
+		cameraOrigRot = Camera.main.transform.rotation;
 	}
+
 	void Update () {
 		if (Input.GetMouseButtonDown (0)) {
 			Vector3 mousePos = Input.mousePosition;
 
-			MaybeLoadLevel (mousePos, cowPalaceLevel, "CowPalace");
+			// See if the user clicked on a level and try to load that level
+			LoadLevelInfo(mousePos, "Cow Palace", "CowPalace");
+
+			//cameraTargetPos = cameraOrigPos;
+			//cameraTargetRot = cameraOrigRot;
+			//StartCoroutine(MoveCameraLoc());
 		}
 	}
+	/*
 	void OnMouseEnter() {
 		iTween.ScaleBy (gameObject, iTween.Hash (
 			"x", SCALE_FACTOR,
@@ -34,19 +49,45 @@ public class SelectLevelScript : MonoBehaviour {
 			"y", 1/SCALE_FACTOR,
 			"z", 1/SCALE_FACTOR,
 			"time", SCALE_TIME));
+	}*/
+
+	/**
+	 * Loads level information if the user clicks on the right spot.
+	 *
+	 * mousePos: Where the user clicked.
+	 * levelAssetsName: Name of all the assets for this level. For example, if the level is called "Level", then there
+	 *                  needs to be objects called "Level Model" and "Level Zoom".
+	 * sceneName: Name of the scene associated with this level.
+	 */
+	bool LoadLevelInfo(Vector3 mousePos, string levelAssetsName, string sceneName) {
+		Vector3 objLoc = Camera.main.WorldToScreenPoint(GameObject.Find(levelAssetsName + " Model").transform.position);
+		if (Mathf.Abs (objLoc.x - mousePos.x) < CLICK_THRESHOLD &&
+		    Mathf.Abs (objLoc.y - mousePos.y) < CLICK_THRESHOLD) {
+
+			// Find the target camera zoom location and move the camera there.
+			GameObject cameraZoomLoc = GameObject.Find(levelAssetsName + " Zoom");
+			StartCoroutine(MoveCameraLoc(cameraZoomLoc));
+
+			// Show level information.
+			//Application.LoadLevel (levelName);
+			return true;
+		}
+		return false;
 	}
 
 	/**
-	 * Possibly loads a level if the user clicks on the right spot.
-	 *
-	 * mousePos: Where the use clicked.
-	 * levelPos: Location of the on-screen object representing the level.
-	 * levelName: Name of the level.
+	 * Moves the camera location to the target position and rotation.
 	 */
-	void MaybeLoadLevel(Vector3 mousePos, Vector3 levelPos, string levelName) {
-		if (Mathf.Abs (levelPos.x - mousePos.x) < CLICK_THRESHOLD &&
-		    Mathf.Abs (levelPos.y - mousePos.y) < CLICK_THRESHOLD) {
-			Application.LoadLevel (levelName);
+	IEnumerator MoveCameraLoc(GameObject target) {
+		float t = 0.0f;
+		Vector3 startingPos = Camera.main.transform.position;
+		Vector3 targetPos = target.transform.position;
+		Quaternion targetRot = target.transform.rotation;
+		while (t < 1.0f) {
+			t += Time.deltaTime * (Time.timeScale / TRANSITION_DURATION);
+			Camera.main.transform.position = Vector3.Lerp(startingPos, targetPos, t);
+			// TODO rotation
+			yield return 0;
 		}
 	}
 }
