02c52b29a04e93e0a22347e7c732cacd14b5440e
Angela Gong <anjoola@anjoola.com>
2015-03-13 16:16:27 -0700

    Fixed audio settings, removed unused assets

--- Assets/Scripts/Global/AllLevelsList.cs
+++ Assets/Scripts/Global/AllLevelsList.cs
@@ -11,12 +11,16 @@ public class AllLevelsList : MonoBehaviour {
 	public static List<Level> getAllLevels() {
 		List<Level> levels = new List<Level>();
 
+		// Cow palace.
 		// (prefix of all related assets, scene name, 5-star score, time limit).
 		levels.Add(new Level("Cow Palace", "CowFlight", (20 - 2) * POINTS_PER_OBJECT, 100));
-		//levels.Add(new Level("Classroom Chaos", "Classroom", 100, 100));
+
+		// Underwater.
 		levels.Add(new Level("Underwater Meal", "SharkWater", 100, 30));
+
+		// Other
+		//levels.Add(new Level("Classroom Chaos", "Classroom", 100, 100));
 		//levels.Add(new Level("Aerial Dangers", "BirdAndPropeller", 100, 100));
-		// TODO add more
 
 		return levels;
 	}
