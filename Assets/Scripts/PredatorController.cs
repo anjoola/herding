f15ea4c864b417e1569f7f144003f3585e5c900e
Dan Guo <danguo1113@gmail.com>
2015-03-14 09:34:48 -0700

    multitouch! and new background image

--- Assets/Scripts/PredatorController.cs
+++ Assets/Scripts/PredatorController.cs
@@ -27,83 +27,86 @@ public class PredatorController : MonoBehaviour {
 
 
 	/******** WORKING for single touch */
-	void OnInputDown(Vector2 mousePosition)
-	{
-		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
-		RaycastHit2D hit = Physics2D.Raycast (Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
-		
-		if (hit.collider.transform.position == collider2D.transform.position)
-		{
-			Debug.Log (hit.collider.gameObject);
-			v3Offset = hit.collider.transform.position - ray.GetPoint (10.0f);
-			v3Offset.z = 300.0f;
-			isMouseDown = true;
-		}
-	}
-
 	void Update(){
+		Debug.Log ("Multitouch enabled?" + Input.multiTouchEnabled);
 		if (Input.touchCount > 0) {
 			Debug.Log ("Num touches: " + Input.touchCount);
 			for (int i = 0; i < Input.touchCount; i++) {
-				
-				Debug.Log ("Checking touch: " + i);
+
 				Touch t = Input.GetTouch (i);
+				Debug.Log ("Checking touch: " + i + "at position: " + t.position);
 
 				Ray ray = Camera.main.ScreenPointToRay (t.position);
 				RaycastHit2D hit = Physics2D.Raycast (Camera.main.ScreenToWorldPoint(t.position), Vector2.zero);
 
 				// No hit
 				if (hit.collider == null) continue;
-				if (hit.collider.transform.position == collider2D.transform.position){
+				if (hit.collider.gameObject == collider2D.gameObject){
 					// hit this particular game object
 
 					Debug.Log (hit.collider.gameObject);
 					if (t.phase == TouchPhase.Began) {
 						Debug.Log ("Began");
-						OnInputDown (t.position);
+						OnInputDown (hit.collider, ray);
 					} else if (t.phase == TouchPhase.Moved || t.phase == TouchPhase.Stationary){
 						Debug.Log ("Drag");
-						OnInputDrag(t.position);
+						OnInputDrag(hit.collider, ray);
 					} else {
 						Debug.Log ("Up");
-						OnInputUp(t.position);
+						OnInputUp(hit.collider, ray);
 					}
 				}
 
 
 			}
 		} else {
-			Debug.Log ("No touches");
-			if (Input.GetMouseButtonDown (0)) {
-				Debug.Log ("Down");
-				OnInputDown (Input.mousePosition);
-			} else if (Input.GetMouseButton (0)) {
-				Debug.Log ("Still Down");
-				OnInputDrag (Input.mousePosition);
-			} else if (Input.GetMouseButtonUp (0)) {
-				Debug.Log ("Up");
-				OnInputUp (Input.mousePosition);
+			if (Input.GetMouseButtonDown (0) || Input.GetMouseButton (0) || Input.GetMouseButtonUp (0)){
+				Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+				RaycastHit2D hit = Physics2D.Raycast (Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+				
+				// No hit
+				if (hit.collider == null) return;
+				if (hit.collider.gameObject == collider2D.gameObject){
+					// hit this particular game object
+					
+					Debug.Log (hit.collider.gameObject);
+					if (Input.GetMouseButtonDown (0)) {
+						Debug.Log ("Down");
+						OnInputDown (hit.collider, ray);
+					} else if (Input.GetMouseButton (0)) {
+						Debug.Log ("Still Down");
+						OnInputDrag (hit.collider, ray);
+					} else if (Input.GetMouseButtonUp (0)) {
+                        Debug.Log ("Up");
+						OnInputUp (hit.collider, ray);
+					}
+                }
+
 			}
 		}
 	}
 
-
-
+	void OnInputDown(Collider2D col, Ray ray)
+	{
+		Debug.Log (col.gameObject);
+		v3Offset = col.transform.position - ray.GetPoint (10.0f);
+		v3Offset.z = 0.0f;
+		isMouseDown = true;
+    }
 	
-	void OnInputUp(Vector2 mousePosition)
+	void OnInputUp(Collider2D col, Ray ray)
 	{
 		isMouseDown = false; 
 	}
 	
-	void OnInputDrag(Vector2 mousePosition)
+	void OnInputDrag(Collider2D col, Ray ray)
 	{
 		if (!isMouseDown) return;
 		if (GlobalStateController.isPaused && !testing) return;
-		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+
 		float dist;
-		plane.Raycast (ray, out dist);
-		Vector3 v3Pos = ray.GetPoint (dist);
-		transform.position = v3Pos + v3Offset; 
+		Vector3 v3Pos = ray.GetPoint (10.0f);
+		transform.position = v3Pos + v3Offset;
 
 		Vector2 predictedDir = rigidbody2D.position - prevPosition;
 		if (predictedDir.magnitude > 2) 
