e541a2c0ee19e973f26babe01cfe337af25993e5
Angela Gong <anjoola@anjoola.com>
2015-03-14 23:15:29 -0700

    Updated world map to include all cow levels

--- Assets/Scripts/Global/AllLevelsList.cs
+++ Assets/Scripts/Global/AllLevelsList.cs
@@ -11,7 +11,10 @@ public class AllLevelsList : MonoBehaviour {
 
 		// Cow palace.
 		// (prefix of all related assets, scene name, 5-star score, time limit).
+		levels.Add(new Level("The Little Red Barn", "CowFlight", (10 - 2) * CowBoid.POINT_PER_BOID, 10));
 		levels.Add(new Level("Cow Palace", "CowFlight", (10 - 2) * CowBoid.POINT_PER_BOID, 10));
+		levels.Add(new Level("Hank's Mini-Barn", "CowFlight", (10 - 2) * CowBoid.POINT_PER_BOID, 10));
+		levels.Add(new Level("Cows Overrun", "CowFlight", (10 - 2) * CowBoid.POINT_PER_BOID, 10));
 
 		// Underwater.
 		levels.Add(new Level("Underwater Meal", "SharkWater", 100, 30));
