7ffa3bf2d6078a017cc122038e51078cb025d3f2
Angela Gong <anjoola@anjoola.com>
2015-03-19 01:24:55 -0700

    Minor change of level

--- Assets/Scripts/Global/AllLevelsList.cs
+++ Assets/Scripts/Global/AllLevelsList.cs
@@ -17,7 +17,7 @@ public class AllLevelsList : MonoBehaviour {
 		levels.Add(new Level("Cows Overrun", "CowOverrun", (12 - 2) * CowBoid.POINT_PER_BOID, 20));
 
 		// Underwater.
-		levels.Add(new Level("Crab Meal", "SharkWater0", 640, 20));
+		levels.Add(new Level("Crab Meal", "SharkWater0", 600, 20));
 		levels.Add(new Level("Rock Cave", "SharkWater1", 640, 10));
 		levels.Add(new Level("Teamwork", "SharkWater2", 100, 15));
 		levels.Add(new Level("Freedom", "SharkWater3", 100, 15));
